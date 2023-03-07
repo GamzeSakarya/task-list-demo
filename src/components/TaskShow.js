6cd3620b9680bfec3cea68488f010715d7dba95a
gamzesakarya <gamzesakarya2017@gmail.com>
2023-03-07 20:09:16 +0300

    first commit

---
+++ src/components/TaskShow.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState } from "react";
+import TaskCreate from "./TaskCreate";
+
+const TaskShow = ({ task, onDelete, onUpdate }) => {
+  const [showEdit, setShowEdit] = useState(false);
+  const handleDeleteClick = () => {
+    onDelete(task.id);
+  };
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+  const handleSubmit = (id, updatedTitle, updatedTaskDesk) => {
+    setShowEdit(false);
+    onUpdate(id, updatedTitle, updatedTaskDesk);
+  };
+
+  return (
+    <div className="task-show">
+      {showEdit ? (
+        <TaskCreate task={task} taskformUpdate={true} onUpdate={handleSubmit} />
+      ) : (
+        <div>
+          <h3 className="task-title">Göreviniz</h3>
+          <p>{task.title}</p>
+          <h3 className="task-title">Yapılacaklar</h3>
+          <p>{task.taskDesc}</p>
+          <div>
+            <button className="task-delete" onClick={handleDeleteClick}>
+              Sil
+            </button>
+            <button className="task-edit" onClick={handleEditClick}>
+              Güncelle
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TaskShow;
